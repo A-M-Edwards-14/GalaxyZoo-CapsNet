914bf50d12ba057abf74954affa4823ecf6d7d02
A-M-Edwards-14 <87430850+A-M-Edwards-14@users.noreply.github.com>
2022-03-26 21:33:48 +0000

    Add files via upload

---
+++ CapsNet/CapsNetPredictor.py
@@ -0,0 +1,140 @@
+import sys
+sys.setrecursionlimit(15000)
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+import numpy as np
+
+BATCH_SIZE = 1
+NUM_CLASSES = 3
+NUM_EPOCHS = 1
+NUM_ROUTING_ITERATIONS = 3
+
+def softmax(input, dim=1):
+    transposed_input = input.transpose(dim, len(input.size()) - 1)
+    softmaxed_output = F.softmax(transposed_input.contiguous().view(-1,
+                                                                    transposed_input.size(-1)), dim=-1)
+    return softmaxed_output.view(*transposed_input.size()).transpose(dim, len(input.size()) - 1)
+
+
+def augmentation(x, max_shift=2):
+    _, _, height, width = x.size()
+
+    h_shift, w_shift = np.random.randint(-max_shift, max_shift + 1, size=2)
+    source_height_slice = slice(max(0, h_shift), h_shift + height)
+    source_width_slice = slice(max(0, w_shift), w_shift + width)
+    target_height_slice = slice(max(0, -h_shift), -h_shift + height)
+    target_width_slice = slice(max(0, -w_shift), -w_shift + width)
+
+    shifted_image = torch.zeros(*x.size())
+    shifted_image[:, :, source_height_slice, source_width_slice] = x[:,
+                                                                     :, target_height_slice, target_width_slice]
+    return shifted_image.float()
+
+
+class CapsuleLayer(nn.Module):
+    def __init__(self, num_capsules, num_route_nodes, in_channels, out_channels, kernel_size=None, stride=None,
+                 num_iterations=NUM_ROUTING_ITERATIONS):
+        super(CapsuleLayer, self).__init__()
+
+        self.num_route_nodes = num_route_nodes
+        self.num_iterations = num_iterations
+
+        self.num_capsules = num_capsules
+
+        if num_route_nodes != -1:
+            self.route_weights = nn.Parameter(torch.randn(
+                num_capsules, num_route_nodes, in_channels, out_channels))
+        else:
+            self.capsules = nn.ModuleList(
+                [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=0) for _ in
+                 range(num_capsules)])
+
+    def squash(self, tensor, dim=-1):
+        squared_norm = (tensor ** 2).sum(dim=dim, keepdim=True)
+        scale = squared_norm / (1 + squared_norm)
+        return scale * tensor / torch.sqrt(squared_norm)
+
+    def forward(self, x):
+        if self.num_route_nodes != -1:
+            priors = x[None, :, :, None, :] @ self.route_weights[:, None, :, :, :]
+
+            logits = Variable(torch.zeros(*priors.size())).cuda()
+            for i in range(self.num_iterations):
+                probs = softmax(logits, dim=2)
+                outputs = self.squash((probs * priors).sum(dim=2, keepdim=True))
+
+                if i != self.num_iterations - 1:
+                    delta_logits = (priors * outputs).sum(dim=-1, keepdim=True)
+                    logits = logits + delta_logits
+        else:
+            outputs = [capsule(x).view(x.size(0), -1, 1) for capsule in self.capsules]
+            outputs = torch.cat(outputs, dim=-1)
+            outputs = self.squash(outputs)
+
+        return outputs
+
+
+class CapsuleNet(nn.Module):
+    def __init__(self):
+        super(CapsuleNet, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=256, kernel_size=9, stride=1)
+        self.primary_capsules = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
+                                             kernel_size=9, stride=2)
+        self.digit_capsules = CapsuleLayer(num_capsules=NUM_CLASSES, num_route_nodes=32 * 28 * 28, in_channels=8,
+                                           out_channels=16)
+
+        self.Linear = nn.Linear(16 * NUM_CLASSES, NUM_CLASSES)
+
+    def forward(self, x, y=None):
+        x = F.relu(self.conv1(x), inplace=True)
+        x = self.primary_capsules(x)
+        x = self.digit_capsules(x).squeeze().transpose(0, 1)
+        x = (x ** 2).sum(dim=-1) ** 0.5
+        # x = self.Linear(x.view(x.size(0), -1))
+
+        return x
+
+
+if __name__ == "__main__":
+    from torch.autograd import Variable
+    from torch.optim import Adam
+
+    model = CapsuleNet()
+    model.load_state_dict(torch.load('../edwardam/SDSS/epochs200/epoch_200.pt'))
+    model.cuda()
+
+    print("# parameters:", sum(param.numel() for param in model.parameters()))
+
+    optimizer = Adam(model.parameters())
+
+
+    #Image data
+    X = np.load('../edwardam/Data/SDSSCustomData.npy')
+
+    data = torch.from_numpy(X).float()
+    data = augmentation(data.float())
+    #data = augmentation(data.float()/255)
+    
+    #di is the number of images that are predicted at one time.
+    di=2
+    I=1
+
+    CapsPred=[]
+    #range must be the size of the dataset divided by di
+    for i in range(0,12820):
+        i*=di
+        #print(i, I*di)
+        #I+=1	
+        print(I*di)
+        datasample = data[i:I*di]
+        datasamplecuda = Variable(datasample).cuda()
+        Prediction = model(datasamplecuda)
+        Prednpy = Prediction.cpu().detach().numpy()
+        print(Prednpy)
+        CapsPred.append(Prednpy)
+        I+=1
+
+    np.save('../edwardam/Output/RGBCapsPredict_3class', CapsPred)
